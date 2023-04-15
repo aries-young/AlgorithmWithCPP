1f53a8e66cc398f0f73c0b8b6d8fb74b0eae1d81
aries-young <84262474+aries-young@users.noreply.github.com>
2023-04-15 16:21:19 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <functional>
 using namespace std;
 
-int main() {
-    return 0;
+template<typename T>
+class MyIter {
+public:
+    typedef T value_type; //内嵌类型声明
+    MyIter(T *p = 0) : m_ptr(p) {}
+    T& operator*() const { return *m_ptr;}
+private:
+    T *m_ptr;
+};
+
+//以迭代器所指对象的类型作为返回类型
+//注意typename是必须的，它告诉编译器这是一个类型
+template<typename MyIter>
+typename MyIter::value_type Func(MyIter iter) {
+    return *iter;
+}
+
+int main(int argc, const  char *argv[]) {
+    MyIter<int> iter(new int(666));
+    std::cout<<Func(iter)<<std::endl;  //print=> 666
 }
