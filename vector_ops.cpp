ddb18e08144c1a4b60303d3a794c236de977d468
aries-young <aries_yhf@163.com>
2023-04-18 23:24:43 +0800

    Update hello.cpp

--- vector_ops.cpp
+++ vector_ops.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool cmp(int num) { return num % 2; }
+
 int main() {
     vector<int> nums;
 
@@ -13,4 +15,22 @@ int main() {
 
     // 打印数组
     copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+
+    // vector to set
+    set<int> st(nums.begin(), nums.end());
+    vector<int> vec;
+    vec.assign(st.begin(), st.end());
+
+
+    // vector to string
+    string s = "hello world";
+    vector<char> char_vec;
+    char_vec.assign(s.begin(), s.end());
+    s.assign(char_vec.begin(), char_vec.end());
+
+    // vector count
+    // count(vec.begin(), vec.end(), value)
+    int cnt = count(nums.begin(), nums.end(), 1);
+    // count_if
+    int cnt = count_if(vec.begin(), vec.end(), cmp);
 }
